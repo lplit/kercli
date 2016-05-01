510178f5781382959eae7d2e356d681721994ba4
lplit <michal.rudek@etu.upmc.fr>
2016-05-01 19:27:35 +0200

    kill request struct and pid array struct

--- inc/kercli.h
+++ inc/kercli.h
@@ -13,21 +13,34 @@
 /* String size for lsmod */
 #define LS_SIZE 2048
 
+/* Used by kill */
+struct killerstruct {
+  int sid; // Signal ID
+  int pid; // Process ID
+};
+
+/* Used by wait */
+struct pid_list {
+  int nb_element;
+  int *array_pointer;
+  int return_value;
+};
+
 /* Copy of /sys/sysinfo.h structure */
 struct mem_infos {
-	long long uptime;				/* Seconds since boot */
-	unsigned long long loads[3];	/* 1, 5, and 15 minute load averages */
-	unsigned long long totalram;	/* Total usable main memory size */
-	unsigned long long freeram;		/* Available memory size */
-	unsigned long long sharedram;	/* Amount of shared memory */
-	unsigned long long bufferram;	/* Memory used by buffers */
-	unsigned long long totalswap;	/* Total swap space size */
-	unsigned long long freeswap;	/* swap space still available */
-	unsigned short procs;           /* Number of current processes */
-	unsigned short pad;	        	/* Explicit padding for m68k */
-	unsigned long long totalhigh;	/* Total high memory size */
-	unsigned long long freehigh;	/* Available high memory size */
-	unsigned long mem_unit;	        /* Memory unit size in bytes */
+  long long uptime;				/* Seconds since boot */
+  unsigned long long loads[3];	/* 1, 5, and 15 minute load averages */
+  unsigned long long totalram;	/* Total usable main memory size */
+  unsigned long long freeram;		/* Available memory size */
+  unsigned long long sharedram;	/* Amount of shared memory */
+  unsigned long long bufferram;	/* Memory used by buffers */
+  unsigned long long totalswap;	/* Total swap space size */
+  unsigned long long freeswap;	/* swap space still available */
+  unsigned short procs;           /* Number of current processes */
+  unsigned short pad;	        	/* Explicit padding for m68k */
+  unsigned long long totalhigh;	/* Total high memory size */
+  unsigned long long freehigh;	/* Available high memory size */
+  unsigned long mem_unit;	        /* Memory unit size in bytes */
 };
 
 #endif 
